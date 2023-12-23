2211de180bff1ed8b36fc1ae785e34e311db530c
Jannah Moussaoui <jannahmoussaoui@gmail.com>
2023-12-23 09:31:33 -0500

    updated R survey error_on_missing_name <- function(name){ var <- Sys.getenv(name, unset=NA) if(is.na(var)){ stop(paste0("cannot find ",name, " !"),call. = FALSE) }

--- NASA_Survey/App.R
+++ NASA_Survey/App.R
@@ -1,6 +1,13 @@
 library(shiny)
 library(shinysurveys)
 library(tibble)
+library(rsconnect)
+# a function to stop the script when one of the variables cannot be found. and to strip quotation marks from the secrets when you supplied them. (maybe it is just easier to never use them)
+error_on_missing_name <- function(name){
+  var <- Sys.getenv(name, unset=NA)
+  if(is.na(var)){
+    stop(paste0("cannot find ",name, " !"),call. = FALSE)
+  }}
 
 if (interactive()) {
   df <- data.frame(
